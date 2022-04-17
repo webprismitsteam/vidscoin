ed2855bf0ce78cf8239b9f571c873b36e2c84a0d
Arnab Munshi <arnab47stars@gmail.com>
2022-04-18 03:31:05 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -167,9 +167,6 @@ export default function Home() {
           </div>
         </div>
       </main>
-
-      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </div>
   )
 }
